71fc58c03f5e26fa0ce76d246188d4f4870e847f
Old <ebhosale73@gmail.com>
2020-10-12 20:08:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+// import FetchData from './FetchData';
+import DataFetch from './DataFetch'
+
+
+function App() {
+  return (
+    <>
+    {/* <FetchData/> */}
+    <DataFetch/>
+    </>
+  );
+}
+
+export default App;
